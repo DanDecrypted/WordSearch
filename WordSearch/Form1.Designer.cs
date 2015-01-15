56d94f20004c37da0798f4529f6a312b8627dcff
Dan Scott <me@idanscott.co.uk>
2015-01-15 15:37:39 +0000

    Word Search
    Initial Commit

---
+++ WordSearch/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WordSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEditWord = new System.Windows.Forms.Button();
+            this.txtEdit = new System.Windows.Forms.TextBox();
+            this.btnAddWord = new System.Windows.Forms.Button();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.lstWords = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGridSize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.wordGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wordGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEditWord);
+            this.groupBox1.Controls.Add(this.txtEdit);
+            this.groupBox1.Controls.Add(this.btnAddWord);
+            this.groupBox1.Controls.Add(this.txtAdd);
+            this.groupBox1.Controls.Add(this.lblWords);
+            this.groupBox1.Controls.Add(this.lstWords);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnGridSize);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtSize);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 428);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grid Settings ";
+            // 
+            // btnEditWord
+            // 
+            this.btnEditWord.Location = new System.Drawing.Point(112, 148);
+            this.btnEditWord.Name = "btnEditWord";
+            this.btnEditWord.Size = new System.Drawing.Size(48, 23);
+            this.btnEditWord.TabIndex = 10;
+            this.btnEditWord.Text = "Save";
+            this.btnEditWord.UseVisualStyleBackColor = true;
+            this.btnEditWord.Click += new System.EventHandler(this.btnEditWord_Click);
+            // 
+            // txtEdit
+            // 
+            this.txtEdit.Location = new System.Drawing.Point(6, 150);
+            this.txtEdit.Name = "txtEdit";
+            this.txtEdit.Size = new System.Drawing.Size(100, 20);
+            this.txtEdit.TabIndex = 9;
+            // 
+            // btnAddWord
+            // 
+            this.btnAddWord.Location = new System.Drawing.Point(112, 98);
+            this.btnAddWord.Name = "btnAddWord";
+            this.btnAddWord.Size = new System.Drawing.Size(48, 23);
+            this.btnAddWord.TabIndex = 8;
+            this.btnAddWord.Text = "Add";
+            this.btnAddWord.UseVisualStyleBackColor = true;
+            this.btnAddWord.Click += new System.EventHandler(this.btnAddWord_Click);
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(6, 100);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd.TabIndex = 7;
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Location = new System.Drawing.Point(6, 188);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(102, 13);
+            this.lblWords.TabIndex = 6;
+            this.lblWords.Text = "Find these 10 words";
+            // 
+            // lstWords
+            // 
+            this.lstWords.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstWords.FormattingEnabled = true;
+            this.lstWords.Location = new System.Drawing.Point(6, 204);
+            this.lstWords.Name = "lstWords";
+            this.lstWords.Size = new System.Drawing.Size(120, 212);
+            this.lstWords.TabIndex = 5;
+            this.lstWords.SelectedIndexChanged += new System.EventHandler(this.lstWords_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Edit existing word...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Add new word...";
+            // 
+            // btnGridSize
+            // 
+            this.btnGridSize.Location = new System.Drawing.Point(112, 43);
+            this.btnGridSize.Name = "btnGridSize";
+            this.btnGridSize.Size = new System.Drawing.Size(48, 23);
+            this.btnGridSize.TabIndex = 2;
+            this.btnGridSize.Text = "GO";
+            this.btnGridSize.UseVisualStyleBackColor = true;
+            this.btnGridSize.Click += new System.EventHandler(this.btnGridSize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter grid size: *Required";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(6, 45);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 0;
+            // 
+            // wordGrid
+            // 
+            this.wordGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.wordGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.wordGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.wordGrid.ColumnHeadersVisible = false;
+            this.wordGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wordGrid.Location = new System.Drawing.Point(168, 0);
+            this.wordGrid.Name = "wordGrid";
+            this.wordGrid.RowHeadersVisible = false;
+            this.wordGrid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.wordGrid.ShowCellToolTips = false;
+            this.wordGrid.ShowEditingIcon = false;
+            this.wordGrid.Size = new System.Drawing.Size(458, 428);
+            this.wordGrid.TabIndex = 11;
+            this.wordGrid.CellStateChanged += new System.Windows.Forms.DataGridViewCellStateChangedEventHandler(this.wordGrid_CellStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 428);
+            this.Controls.Add(this.wordGrid);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Word Search ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wordGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEditWord;
+        private System.Windows.Forms.TextBox txtEdit;
+        private System.Windows.Forms.Button btnAddWord;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.ListBox lstWords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGridSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.DataGridView wordGrid;
+    }
+}
+
---
+++ WordSearch/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordSearch
+{
+    public partial class Form1 : Form
+    {
+        private List<string> m_WordsToFind;
+        private int m_PreviousSelection;
+        private int m_GridSize;
+        private bool m_GridBuilt;
+        private List<string> m_WordsFound;
+        public Form1()
+        {
+            InitializeComponent();
+            m_WordsFound = new List<string>();
+            m_WordsToFind = new List<string>(new string[]{ "integer", "array", "byte", "boolean", "for", "string", "parameter", "while", "method", "loop" });
+            m_PreviousSelection = 0;
+            m_GridBuilt = false;
+        }
+
+        private string ReversedString(string input)
+        {
+            string temp = "";
+            foreach (char c in input)
+            {
+                temp = c + temp;
+            }
+            return temp;
+        }
+
+        private void RefreshWords()
+        {
+            if (lstWords.SelectedIndex != null)
+            {
+                m_PreviousSelection = lstWords.SelectedIndex;
+            }
+
+            lstWords.Items.Clear();
+
+            foreach (string wordToFind in m_WordsToFind)
+            {
+                lstWords.Items.Add(wordToFind);
+            }
+
+            lblWords.Text = "Find these " + m_WordsToFind.Count.ToString() + " words";
+
+            lstWords.SelectedIndex = m_PreviousSelection;
+
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            //initialise grid
+            m_WordsFound = new List<string>();
+            m_GridBuilt = false;
+            wordGrid.Columns.Clear();
+            wordGrid.Rows.Clear();
+            if (txtSize.Text != "" && txtSize != null)
+            {
+                m_GridSize = Convert.ToInt32(txtSize.Text);
+                for (int i = 0; i < m_GridSize; i++)
+                {
+                    wordGrid.Columns.Add("x" + i.ToString(), "x" + i.ToString());
+                }
+                wordGrid.Rows.Add(m_GridSize - 1);
+
+                Random rndNumber = new Random();
+                foreach (string word in m_WordsToFind)
+                {
+                    bool wordInserted = false;
+                    while (!wordInserted)
+                    {
+                        int xStart = rndNumber.Next(0, m_GridSize - 1);
+                        int yStart = rndNumber.Next(0, m_GridSize - 1);
+                        int direction = rndNumber.Next(1, 5);
+                        wordInserted = InsertWord(direction, word, xStart, yStart, true);
+                    }
+                }
+
+                for (int x = 0; x < m_GridSize; x++)
+                {
+                    for (int y = 0; y < m_GridSize; y++)
+                    {
+                        if (wordGrid[x, y].Value == null)
+                            wordGrid[x, y].Value = (char)('a' + rndNumber.Next(0, 26));
+                    }
+                }
+            }
+            m_GridBuilt = true;
+        }
+
+        private bool InsertWord(int direction, string word, int x, int y, bool checkFit)
+        {
+            bool canFit = true;
+            int xWrapSafe = x;
+            int yWrapSafe = y;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (direction == 1)
+                {
+                    if (x - i < 0)
+                        xWrapSafe = m_GridSize + (x - i);
+                    else
+                        xWrapSafe = x - i;
+                }
+                //down
+                else if (direction == 2)
+                {
+                    if (y + i >= m_GridSize)
+                        yWrapSafe = 0 + ((y + i) - m_GridSize);
+                    else
+                        yWrapSafe = y + i;
+                }
+                //right
+                if (direction == 3)
+                {
+                    if (x + i >= m_GridSize)
+                        xWrapSafe = 0 + ((x + i) - m_GridSize);
+                    else
+                        xWrapSafe = x + i;
+                }
+                //up
+                else if (direction == 4)
+                {
+                    if (y - i < 0)
+                        yWrapSafe = (m_GridSize + (y - i));
+                    else
+                        yWrapSafe = y - i;
+                }
+
+                if (checkFit)
+                {
+                    if (wordGrid[xWrapSafe, yWrapSafe].Value != null)
+                    {
+                        canFit = false;
+                    }
+                }
+                else
+                    wordGrid[xWrapSafe, yWrapSafe].Value = word[i];
+            }
+
+            if (checkFit && canFit)
+            {
+                //checked that the word can fit, so now we can put it in 
+                InsertWord(direction, word, x, y, false);
+                System.Diagnostics.Debug.Write("Word " + word + " inserted x:" + x + " y:" + y + " direction:" + direction + " \n");
+            }
+            return canFit;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshWords();
+            lstWords.SelectedIndex = 0;
+        }
+
+        private void btnAddWord_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text != "" && txtAdd.Text != null)
+            {
+                m_WordsToFind.Add(txtAdd.Text);
+                RefreshWords();
+            }
+        }
+
+        private void lstWords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtEdit.Text = lstWords.Items[lstWords.SelectedIndex].ToString();
+        }
+
+        private void btnEditWord_Click(object sender, EventArgs e)
+        {
+            if (txtEdit.Text != "" && txtEdit.Text != null)
+            {
+                m_WordsToFind[lstWords.SelectedIndex] = txtEdit.Text;
+                RefreshWords();
+            }
+        }
+
+        private void btnGridSize_Click(object sender, EventArgs e)
+        {
+            CreateGrid();
+        }
+
+        private void wordGrid_CellStateChanged(object sender, DataGridViewCellStateChangedEventArgs e)
+        {
+            if (m_GridBuilt)
+            {
+                string selectedWord = "";
+                foreach (DataGridViewCell cell in wordGrid.SelectedCells)
+                {
+                    selectedWord += cell.Value.ToString();
+                }
+
+                foreach (string word in m_WordsToFind)
+                {
+                    if ((word == selectedWord) || (word == ReversedString(selectedWord)))
+                    {
+                        foreach (DataGridViewCell cell in wordGrid.SelectedCells)
+                        {
+                            cell.Style.BackColor = Color.Green;
+                            if (!m_WordsFound.Contains(word))
+                            {
+                                m_WordsFound.Add(word);
+                                MessageBox.Show("Congratulations, You have found \"" + word + "\". " + m_WordsFound.Count.ToString() + "/" + m_WordsToFind.Count.ToString(), "Word Found!");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
